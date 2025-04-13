5674e8c0e94a26562f46ead0dd54a153d087bb94
Caio Cabral <caiocabral.ep@gmail.com>
2025-04-13 19:53:10 -0300

    Primeiro Commit

---
+++ src/test/java/com/programmer/caiocabral/api/primeira/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmer.caiocabral.api.primeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
