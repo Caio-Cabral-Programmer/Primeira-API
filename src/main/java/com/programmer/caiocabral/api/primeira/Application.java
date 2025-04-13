8ca19c496abe30db5c1b7af0653565abc8673f5a
Caio Cabral <caiocabral.ep@gmail.com>
2025-04-13 20:28:13 -0300

    Hello, World!

--- src/main/java/com/programmer/caiocabral/api/primeira/Application.java
+++ src/main/java/com/programmer/caiocabral/api/primeira/Application.java
@@ -2,6 +2,7 @@ package com.programmer.caiocabral.api.primeira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class Application {
